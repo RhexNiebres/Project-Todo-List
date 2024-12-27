8094fba85078360292a5aa2e30e8da57364bd065
RhexNiebres <rhexniebres@gmail.com>
2024-12-27 14:02:57 -0800

    add minimal css styling for aesthetics of the app.

--- webpack-practice/src/deleteTask.js
+++
@@ -1,6 +0,0 @@
-export function deleteTask(activeFolder, task) {
-    const index = activeFolder.tasks.indexOf(task);
-    if (index > -1) {
-      activeFolder.tasks.splice(index, 1); // Remove the task from the folder
-    }
-  }
